be90b74564799bd8459dd943248455709e6adee1
Fernando Almeida <fernandoalmeida@fisica.ufc.br>
2024-03-05 09:37:17 -0300

    Add files via upload

---
+++ src/components/ContentTop/InitialContent/InitialContent.tsx
@@ -0,0 +1,13 @@
+import { Styles } from "./InitialContent.styles";
+
+export default function InitialContent() {
+  return (
+    <Styles.TitleText>
+      Um ponto
+      <br />
+      fora da
+      <br />
+      curva.
+    </Styles.TitleText>
+  );
+}
