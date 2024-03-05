f4b2f28f76886620cbdfb62b75357740419ede1f
Fernando Almeida <fernandoalmeida@fisica.ufc.br>
2024-03-05 10:34:45 -0300

    Update middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -8,7 +8,7 @@ export function middleware(request: NextRequest) {
   const cspHeader = `
     default-src 'self';
     script-src 'self' 'nonce-${nonce}' 'strict-dynamic';
-    style-src 'self' 'unsafe-inline';
+    style-src 'self' 'nonce-${nonce}';
     img-src 'self' blob: data:;
     font-src 'self';
     object-src 'none';
@@ -43,3 +43,22 @@ export function middleware(request: NextRequest) {
  
   return response
 }
+
+export const config = {
+    matcher: [
+      /*
+       * Match all request paths except for the ones starting with:
+       * - api (API routes)
+       * - _next/static (static files)
+       * - _next/image (image optimization files)
+       * - favicon.ico (favicon file)
+       */
+      {
+        source: '/((?!api|_next/static|_next/image|favicon.ico).*)',
+        missing: [
+          { type: 'header', key: 'next-router-prefetch' },
+          { type: 'header', key: 'purpose', value: 'prefetch' },
+        ],
+      },
+    ],
+  }
