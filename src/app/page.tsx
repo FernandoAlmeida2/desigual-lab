b9c719d5f6bbe35ffa52e57bca99aafbc78335ab
Fernando Almeida <fernandoalmeida@fisica.ufc.br>
2024-03-05 10:49:37 -0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,6 @@ import ContentTop from "@/components/ContentTop/ContentTop";
 import DesigualMap from "@/components/DesigualMap/DesigualMap";
 import { useSelector } from "react-redux";
 import { RootState } from "@/app/lib/store";
-import Script from "next/script";
-import { headers } from "next/headers";
 
 /* declare global {
   interface Window {
@@ -25,7 +23,6 @@ export default function Home() {
   //const [currentTopic, setCurrentTopic] = useState("initial");
   const ref = useRef<HTMLDivElement>(null);
   const { currentTopic } = useSelector((state: RootState) => state.topic);
-  const nonce = headers().get("x-nonce");
 
   /* function changeTopic(topic: string) {
     setCurrentTopic(topic);
@@ -95,66 +92,6 @@ export default function Home() {
         </Styles.MainContainer>
       )}
       <Footer />
-      <Script
-          src="/_next/static/chunks/fd9d1056-28d8581e6e02654e.js"
-          strategy="afterInteractive"
-          nonce={nonce!}
-        />
-        <Script
-          src="/_next/static/chunks/69-05e2b4d6c8006538.js"
-          strategy="afterInteractive"
-          nonce={nonce!}
-        />
-        <Script
-          src="/_next/static/chunks/main-app-f5c8d25c8479457f.js"
-          strategy="afterInteractive"
-          nonce={nonce!}
-        />
-        <Script
-          src="/_next/static/chunks/578c2090-075eb9318a86d41b.js"
-          strategy="afterInteractive"
-          nonce={nonce!}
-        />
-        <Script
-          src="/_next/static/chunks/30a37ab2-f3a096e8c768c331.js"
-          strategy="afterInteractive"
-          nonce={nonce!}
-        />
-        <Script
-          src="/_next/static/chunks/94730671-27906ffb01048816.js"
-          strategy="afterInteractive"
-          nonce={nonce!}
-        />
-        <Script
-          src="/_next/static/chunks/795d4814-283306e60742f68d.js"
-          strategy="afterInteractive"
-          nonce={nonce!}
-        />
-        <Script
-          src="/_next/static/chunks/8e1d74a4-0c5a13bbc1871c7b.js"
-          strategy="afterInteractive"
-          nonce={nonce!}
-        />
-        <Script
-          src="/_next/static/chunks/181-24897ff0e2c152aa.js"
-          strategy="afterInteractive"
-          nonce={nonce!}
-        />
-        <Script
-          src="/_next/static/chunks/835-229fe9d9298440c5.js"
-          strategy="afterInteractive"
-          nonce={nonce!}
-        />
-        <Script
-          src="/_next/static/chunks/app/page-6544158b858fdfe0.js"
-          strategy="afterInteractive"
-          nonce={nonce!}
-        />
-        <Script
-          src="/_next/static/chunks/app/layout-eb6ca078cf5be9c9.js"
-          strategy="afterInteractive"
-          nonce={nonce!}
-        />
     </>
   );
 }
