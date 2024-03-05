395ad73ec04b487994ddd2edd785da597a06058a
Fernando Almeida <fernandoalmeida@fisica.ufc.br>
2024-03-05 10:37:31 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,61 +24,73 @@ export default function RootLayout({
         <Script
           src="/_next/static/chunks/fd9d1056-28d8581e6e02654e.js"
           strategy="afterInteractive"
+          async={true}
           nonce={nonce!}
         />
         <Script
           src="/_next/static/chunks/69-05e2b4d6c8006538.js"
           strategy="afterInteractive"
+          async={true}
           nonce={nonce!}
         />
         <Script
           src="/_next/static/chunks/main-app-f5c8d25c8479457f.js"
           strategy="afterInteractive"
+          async={true}
           nonce={nonce!}
         />
         <Script
           src="/_next/static/chunks/578c2090-075eb9318a86d41b.js"
           strategy="afterInteractive"
+          async={true}
           nonce={nonce!}
         />
         <Script
           src="/_next/static/chunks/30a37ab2-f3a096e8c768c331.js"
           strategy="afterInteractive"
+          async={true}
           nonce={nonce!}
         />
         <Script
           src="/_next/static/chunks/94730671-27906ffb01048816.js"
           strategy="afterInteractive"
+          async={true}
           nonce={nonce!}
         />
         <Script
           src="/_next/static/chunks/795d4814-283306e60742f68d.js"
           strategy="afterInteractive"
+          async={true}
           nonce={nonce!}
         />
         <Script
           src="/_next/static/chunks/8e1d74a4-0c5a13bbc1871c7b.js"
           strategy="afterInteractive"
+          async={true}
           nonce={nonce!}
         />
         <Script
           src="/_next/static/chunks/181-24897ff0e2c152aa.js"
           strategy="afterInteractive"
+          async={true}
           nonce={nonce!}
         />
         <Script
           src="/_next/static/chunks/835-229fe9d9298440c5.js"
           strategy="afterInteractive"
+          async={true}
           nonce={nonce!}
         />
         <Script
           src="/_next/static/chunks/app/page-6544158b858fdfe0.js"
           strategy="afterInteractive"
+          async={true}
           nonce={nonce!}
         />
         <Script
           src="/_next/static/chunks/app/layout-eb6ca078cf5be9c9.js"
           strategy="afterInteractive"
+          async={true}
           nonce={nonce!}
         />
       </head>
