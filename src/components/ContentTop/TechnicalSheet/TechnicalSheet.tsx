fbc3664335b5e1d6bb5a61c4d8ff15eac1d3c6c7
Fernando Almeida <fernandoalmeida@fisica.ufc.br>
2024-03-07 10:45:58 -0300

    Add files via upload

---
+++ src/components/ContentTop/TechnicalSheet/TechnicalSheet.tsx
@@ -0,0 +1,28 @@
+import { Styles } from "../ContentTop.styles";
+
+export default function TechnicalSheet() {
+  return (
+    <Styles.ContentTop>
+      <h1>Ficha Técnica</h1>
+      <h2>Página Web do Desigual Lab</h2>
+      <p>
+        Concepção: Elisângela Teixeira e Élcio Batista
+        <br />
+        Coordenação de desenvolvimento: Fernando Almeida
+        <br />
+        UX/UI Designer: Jaizza Figueiredo
+      </p>
+      <br />
+      <h2>Mapa das Desigualdades</h2>
+      <p>
+        Concepção: Elisângela Teixeira, Felipe Franklin<br />
+        Análise e processamento de Dados: Pedro Florêncio, Anderson Bezerra,
+        Rômulo Andrade
+        <br />
+        Desenvolvimento: Pedro Florêncio, Fernando Almeida
+        <br />
+        UX/UI Designer: Jaizza Figueiredo
+      </p>
+    </Styles.ContentTop>
+  );
+}
