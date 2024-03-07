2095524400433c868c66de63338709ef4b7ca01e
Fernando Almeida <fernandoalmeida@fisica.ufc.br>
2024-03-07 10:00:17 -0300

    Add files via upload

---
+++ src/components/DesigualMap/MapContent/MapContent.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { ChangeEvent, useState } from "react";
+import { Styles } from "./mapContent.style";
+import { IFRAME_BASE_URL } from "@/app/constants/urls";
+
+type Props = {
+  mapPath: string;
+  tablePath: string;
+};
+
+export default function MapContent({ mapPath, tablePath }: Props) {
+  const [itemSelected, setItemSelected] = useState("Mapa");
+
+  return (
+    <Styles.Container>
+      <Styles.SelectItem
+          value={itemSelected}
+          onChange={(e: ChangeEvent<HTMLSelectElement>) =>
+            setItemSelected(e.target.value)
+          }
+          required
+        >
+          <option value="Mapa" selected>
+            Mapa
+          </option>
+          <option value="Tabela">
+            Tabela
+          </option>
+        </Styles.SelectItem>
+      {itemSelected === "Mapa" ? (
+        <iframe
+          id="iframe-mapa"
+          src={`${IFRAME_BASE_URL}/${mapPath}`}
+          width="100%"
+          height="100%"
+          style={{ background: "#d5e9fc", padding: "4vw"}}
+        ></iframe>
+      ) : (
+        <iframe
+          id="iframe-table"
+          src={`${IFRAME_BASE_URL}/${tablePath}`}
+          width="100%"
+          height="100%"
+          style={{ background: "#FFFCDC", padding: "2vw", border: "0.1vw solid #d5d5d5"}}
+        ></iframe>
+      )}
+    </Styles.Container>
+  );
+}
